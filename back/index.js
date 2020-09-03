19c7567fb501e1ec6b5cca14b8ab5c809e08b928
Tiago Carmo <tiago.carmo@gupy.com.br>
2020-09-03 19:14:41 -0300

    back: Sets telemetry

--- back/index.js
+++ back/index.js
@@ -1,15 +1,17 @@
-
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-app.use(cors())
-app.get('/', (req, res) => {
-  const { name } = req.query
-  res.json(`Hello ${name}!`)
-})
+const manager = (name) => `Hello ${name}!`;
+const action = (req, res) => {
+  const { name } = req.query;
+  res.json(manager(name));
+};
+
+app.use(cors());
+app.get('/', action);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Dependencies
+Install dependencies using NPM in the back and front directories
+`npm install`
+
+
+
+## Running Example App 
+### Run the backend
+In backend root directory `/back/` run:
+
+`npm run start`
+
+### Run the frontend
+In frontend root directory `/front/` run: 
+
+`npm run start`
+
+
+
+## Running the Telemetry
+### Dependencies
+Start the Tracing backend (Zipkin)
+
+`docker run --rm -d -p 9411:9411 --name zipkin openzipkin/zipkin`
+
+Access it over: `http://localhost:9411/zipkin/`
+
+
