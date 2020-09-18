82aa05ed9f2859c2fb7df6f399297468563165e9
Joao Guilherme <jgmenezes@live.com>
2020-09-18 11:06:53 -0300

    feat: install exporter collector, remove synchronous operations from front and use CollectorTraceExporter at front and back projets

--- front/src/tracing.js
+++ front/src/tracing.js
@@ -1,47 +1,27 @@
-import { SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { WebTracerProvider } from '@opentelemetry/web';
 import { DocumentLoad } from '@opentelemetry/plugin-document-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { UserInteractionPlugin } from '@opentelemetry/plugin-user-interaction';
-import { XMLHttpRequestPlugin } from '@opentelemetry/plugin-xml-http-request';
-import {ZipkinExporter} from "@opentelemetry/exporter-zipkin";
-
-// Supports only synchronous operations
-const provider = new WebTracerProvider({
-    plugins: [
-        new DocumentLoad(),
-        new UserInteractionPlugin(),
-        new XMLHttpRequestPlugin({
-            // this is webpack  auto reload - we can ignore it
-            ignoreUrls: [/localhost:8091\/sockjs-node/],
-            propagateTraceHeaderCorsUrls: '*',
-        }),
-    ]
-});
-
-provider.addSpanProcessor(new SimpleSpanProcessor(
-    new ZipkinExporter({
-        serviceName: "front-ats",
-    })
-    // Use `new ConsoleSpanExporter()` to log on console
-));
-provider.register();
-
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 
 // Supports asynchronous operations
-const providerWithZone = new WebTracerProvider({
+const provider = new WebTracerProvider({
     plugins: [
         new DocumentLoad()
     ]
 });
-providerWithZone.addSpanProcessor(new SimpleSpanProcessor(
-    new ZipkinExporter({
-        serviceName: "front-ats",
-    })
-    // Use `new ConsoleSpanExporter()` to log on console
+
+provider.addSpanProcessor(new SimpleSpanProcessor(
+    new ConsoleSpanExporter(),
 ));
 
-providerWithZone.register({
+provider.addSpanProcessor(new SimpleSpanProcessor(
+    new CollectorTraceExporter({
+        serviceName: 'front-ats'
+    }),
+));
+
+provider.register({
     contextManager: new ZoneContextManager().enable(),
 });
 
